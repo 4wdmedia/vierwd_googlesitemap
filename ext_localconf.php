aa7e0b1fe945cfa68d09cf924a64746f205e3694
Robert Vock <robert.vock@4wdmedia.de>
2017-03-30 16:05:35 +0200

    fixed content-type headers in TYPO3 6.2

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,3 +7,8 @@ $extensionNamespace = 'Vierwd\\' . str_replace(' ', '', ucwords(str_replace('_',
 // ***************
 // Realurl config
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/realurl/class.tx_realurl_autoconfgen.php']['extensionConfiguration'][$_EXTKEY] = $extensionNamespace . '\\Hooks\\Realurl->addConfig';
+
+if (TYPO3_version < '7.0.0') {
+	$typoscriptFile = \TYPO3\CMS\Core\Utility\GeneralUtility::getFileAbsFileName('EXT:' . $_EXTKEY . '/ext_typoscript_setup-6.2.txt');
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript($_EXTKEY, 'setup', file_get_contents($typoscriptFile));
+}
