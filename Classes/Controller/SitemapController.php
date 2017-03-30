da976abd4defc39b0b67b89e10ee184737f57b3c
Robert Vock <robert.vock@4wdmedia.de>
2017-03-30 15:56:14 +0200

    Added support for TYPO3 6.2, also get subpages of folders

--- Classes/Controller/SitemapController.php
+++ Classes/Controller/SitemapController.php
@@ -19,17 +19,21 @@ class SitemapController {
 
 	public function generateAction($content, array $params) {
 		$this->settings = $params['settings.'];
+		$this->settings['includeDoktypes'] = GeneralUtility::intExplode(',', $this->settings['includeDoktypes']);
 
 		$typolinkRenderer = GeneralUtility::makeInstance(ContentObjectRenderer::class);
 
 		$this->urls = [];
+
+		$GLOBALS['TSFE']->gr_list = '0,-1';
 
 		$page = $GLOBALS['TSFE']->sys_page->getPage($GLOBALS['TSFE']->rootLine[0]['uid']);
 		$result = '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 		$result .= $this->renderPages([$page]);
 
 		foreach ($this->settings['additionalLinks.'] as $configuration) {
-			$records = $this->cObj->getRecords($configuration['records.']['table'], $configuration['records.']['select.']);
+			$records = $this->getRecords($configuration['records.']['table'], $configuration['records.']['select.']);
+
 			$typolinkConfiguration = $configuration['typolink.'];
 			$typolinkConfiguration['returnLast'] = 'url';
 			$typolinkConfiguration = ['typolink.' => $typolinkConfiguration];
@@ -52,30 +56,80 @@ class SitemapController {
 		return $result;
 	}
 
+	protected function getRecords($tableName, array $queryConfiguration) {
+		$records = [];
+
+		$res = $this->cObj->exec_getQuery($tableName, $queryConfiguration);
+
+		while (($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) !== false) {
+
+			// Versioning preview:
+			$GLOBALS['TSFE']->sys_page->versionOL($tableName, $row, true);
+
+			// Language overlay:
+			if (is_array($row) && $GLOBALS['TSFE']->sys_language_contentOL) {
+				$row = $GLOBALS['TSFE']->sys_page->getRecordOverlay(
+					$tableName,
+					$row,
+					$GLOBALS['TSFE']->sys_language_content,
+					$GLOBALS['TSFE']->sys_language_contentOL
+				);
+			}
+
+			// Might be unset in the sys_language_contentOL
+			if (is_array($row)) {
+				$records[] = $row;
+			}
+		}
+		$GLOBALS['TYPO3_DB']->sql_free_result($res);
+
+		return $records;
+	}
+
 	protected function renderPages(array $pages = []) {
 		$result = '';
 		foreach ($pages as $page) {
-			if ($GLOBALS['TSFE']->sys_language_uid && !$page['_PAGES_OVERLAY']) {
-				continue;
+			if (in_array($page['doktype'], $this->settings['includeDoktypes'])) {
+				if (!$GLOBALS['TSFE']->sys_language_uid || $page['_PAGES_OVERLAY']) {
+					$url = $this->cObj->typoLink_URL([
+						'parameter' => $page['uid'],
+						'forceAbsoluteUrl' => true,
+					]);
+					if ($url && !in_array($url, $this->urls)) {
+						$this->urls[] = $url;
+
+						$result .= '<url><loc>' . $url . '</loc>';
+						if ($page['SYS_LASTCHANGED']) {
+							$result .= '<lastmod>' . date('c', $page['SYS_LASTCHANGED']) . '</lastmod>';
+						}
+						$result .= '</url>';
+					}
+				}
 			}
 
-			$url = $this->cObj->typoLink_URL([
-				'parameter' => $page['uid'],
-				'forceAbsoluteUrl' => true,
-			]);
-			$this->urls[] = $url;
-
-			$result .= '<url><loc>' . $url . '</loc>';
-			if ($page['SYS_LASTCHANGED']) {
-				$result .= '<lastmod>' . date('c', $page['SYS_LASTCHANGED']) . '</lastmod>';
-			}
-			$result .= '</url>';
-
-			$subpages = $GLOBALS['TSFE']->sys_page->getMenu($page['uid'], '*', 'sorting', 'AND no_search=0 AND doktype IN (' . $this->settings['includeDoktypes'] . ')');
+			$subpages = $this->getSubpages($page['uid']);
 			$result .= $this->renderPages($subpages);
 		}
 
 		return $result;
+	}
+
+	protected function getSubpages($pageUid) {
+		$pages = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows(
+			'*',
+			'pages',
+			'pid=' . (int)$pageUid . ' AND no_search=0' . $GLOBALS['TSFE']->sys_page->enableFields('pages'),
+			'',
+			'sorting'
+		);
+		foreach ($pages as &$page) {
+			$GLOBALS['TSFE']->sys_page->versionOL('pages', $page);
+			if (is_array($page)) {
+				$page = $GLOBALS['TSFE']->sys_page->getPageOverlay($page);
+			}
+		}
+
+		return $pages;
 	}
 
 	public function robotsAction($content, array $params) {
